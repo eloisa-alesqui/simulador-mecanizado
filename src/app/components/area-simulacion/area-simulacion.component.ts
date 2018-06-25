dda0f3ed903a6817c19e7ac5692f30fffe916005
Eloísa Alés Esquivel <eloisa.alesqui@gmail.com>
2018-06-25 23:12:37 +0200

    Se le da forma a la herramienta

--- src/app/components/area-simulacion/area-simulacion.component.ts
+++ src/app/components/area-simulacion/area-simulacion.component.ts
@@ -27,82 +27,119 @@ export class AreaSimulacionComponent implements OnInit {
     this.subscription = this.desplazamientoService.getDesplazamiento().subscribe(desplazamiento => {
 
       // Se procede al movimiento de la herramienta
-      let herramienta: SVGRectElement = this.herramienta.nativeElement;
+      let herramienta: SVGPolygonElement = this.herramienta.nativeElement;
 
       let desplazamientoX: number = desplazamiento.x;
       let desplazamientoY: number = desplazamiento.y;
 
       // Se calcula el tiempo que durara el desplazamiento
-      let desplazamientoLongitud: number = Math.sqrt(Math.pow(desplazamiento.x, 2) + Math.pow(desplazamiento.y, 2));
+      let desplazamientoLongitud: number = Math.sqrt(Math.pow(desplazamientoX, 2) + Math.pow(desplazamientoY, 2));
       let desplazamientoVelocidad: number = this.velocidadService.getVelocidad();
       let desplazamientoTiempo = desplazamientoLongitud / desplazamientoVelocidad;
 
-      // Se aplica el tiempo a la animacion de la herramienta
-      herramienta.style.transitionDuration = desplazamientoTiempo + 's';
-
-
-      let posicionXInicial: number = parseFloat(herramienta.getAttribute("x"));
+      // Posicion inicial y final de la maquina en el desplazamiento
+      let posicionXInicial: number = parseFloat(herramienta.getAttribute('points').split(' ')[0].split(',')[0]);
       let posicionXFinal: number = desplazamientoX + posicionXInicial;
-      herramienta.setAttribute('x', posicionXFinal.toString());
-      let posicionYInicial: number = parseFloat(herramienta.getAttribute("y"));
+      let posicionYInicial: number = parseFloat(herramienta.getAttribute('points').split(' ')[0].split(',')[1]);
       let posicionYFinal: number = desplazamiento.y + posicionYInicial;
-      herramienta.setAttribute('y', posicionYFinal.toString());
 
       // Se procede al borrado de la pieza
       let canvas: HTMLCanvasElement = this.areaSimulacionCanvas.nativeElement;
       let context: CanvasRenderingContext2D = canvas.getContext('2d');
 
-      let requestAnimationFrame =
-        function (callback: any): number {
-          return setTimeout(callback, 1);
-        };
-
-      let goma: Goma = {
-        'x': posicionXInicial,
-        'y': posicionYInicial,
-        'width': parseFloat(herramienta.getAttribute("width")),
-        'height': parseFloat(herramienta.getAttribute("height"))
-      };
-
       // Variables usadas en la actualizacion de la posicion
+      let points:string = herramienta.getAttribute('points');
       let areaSimulacion: HTMLDivElement = this.areaSimulacion.nativeElement;
       let anchoAreaSimulacion: number = areaSimulacion.offsetWidth;
       let altoAreaSimulacion: number = areaSimulacion.offsetHeight;
       let actualizacionPosicionHerramientaService: ActualizacionPosicionHerramientaService = this.actualizacionPosicionHerramientaService;
 
-      let render = function (): void {
-        context.fillStyle = "rgb(255,255,255)";
-        context.fillRect(goma.x, goma.y, goma.width, goma.height);
-        requestAnimationFrame(render);
-      };
-      render();
+      let actualizacionHecha: boolean = false;
+      let i: number = 0;
 
-      let animate = function (prop: string, val: number, duration: number): void {
+      let requestAnimationFrame = function (callback: any): number {
+        return setTimeout(callback, 1);
+      };
+
+      let animate = function (duration: number): void {
         // Calculos requeridos para la funcion step
         let start: number = new Date().getTime();
         let end: number = start + duration;
-        let current: number = goma[prop];
-        let distance: number = val - current;
-
-        let i: number = 0;
 
         let step = function (): void {
           let timestamp: number = new Date().getTime();
           let progress: number = Math.min((duration - (end - timestamp)) / duration, 1);
-          goma[prop] = current + (distance * progress);
           if (progress < 1) {
+
+            // Se actualizan los puntos y se procede al borrado
+            context.fillStyle = "rgb(255,255,255)";
+            context.beginPath();
+            let primerPunto: boolean = true;
+            let pointsStringInicial: string = points;
+            let pointsStringFinal: string = '';
+            let pointsArrayStringInicial: string[] = pointsStringInicial.split(' ');
+            pointsArrayStringInicial.forEach(function (pointStringInicial: string) {
+              let pointInicial: string[] = pointStringInicial.split(',');
+              let xInicial: number = parseFloat(pointInicial[0]);
+              let yInicial: number = parseFloat(pointInicial[1]);
+              let xFinal: number = xInicial + desplazamientoX * progress;
+              let yFinal: number = yInicial + desplazamientoY * progress;
+              pointsStringFinal = pointsStringFinal + xFinal + ',' + yFinal + ' ';
+              if (primerPunto) {
+                context.moveTo(xFinal, yFinal);
+                primerPunto = false;
+              } else {
+                context.lineTo(xFinal, yFinal);
+              }
+            });
+            pointsStringFinal = pointsStringFinal.trim();
+            herramienta.setAttribute('points', pointsStringFinal);
+            context.closePath();
+            context.fill();
+
             // Se actualiza la posicion de la herramienta cada 10 saltos
             if (i % 10 == 0) {
+              let posicionX: number = parseFloat(points.split(' ')[0].split(',')[0]) +  (desplazamientoX * progress);
+              let posicionY: number = parseFloat(points.split(' ')[0].split(',')[1]) +  (desplazamientoY * progress);
               actualizacionPosicionHerramientaService.sendActualizacionPosicionHerramienta(
-                altoAreaSimulacion - goma.y, goma.x);
+                altoAreaSimulacion - posicionY, posicionX);
             }
             i++;
             requestAnimationFrame(step);
           } else {
-            // Se actualiza la posicion de la herramienta al final de la animacion
-            // para que quede correcta
-            actualizacionPosicionHerramientaService.sendActualizacionPosicionHerramienta(
-              altoAreaSimulacion - posicionYFinal, posicionXFinal);
+            if (!actualizacionHecha) {
+              // Se actualiza la posicion de la herramienta al final de la animacion
+              // para que quede correcta
+              actualizacionPosicionHerramientaService.sendActualizacionPosicionHerramienta(
+                altoAreaSimulacion - posicionYFinal, posicionXFinal);
+              // Se actualizan los puntos y se procede al borrado
+              context.fillStyle = "rgb(255,255,255)";
+              context.beginPath();
+              let primerPunto: boolean = true;
+              let pointsStringInicial: string = points;
+              let pointsStringFinal: string = '';
+              let pointsArrayStringInicial: string[] = pointsStringInicial.split(' ');
+              pointsArrayStringInicial.forEach(function (pointStringInicial: string) {
+                let pointInicial: string[] = pointStringInicial.split(',');
+                let xInicial: number = parseFloat(pointInicial[0]);
+                let yInicial: number = parseFloat(pointInicial[1]);
+                let xFinal: number = xInicial + desplazamientoX;
+                let yFinal: number = yInicial + desplazamientoY;
+                pointsStringFinal = pointsStringFinal + xFinal + ',' + yFinal + ' ';
+                if (primerPunto) {
+                  context.moveTo(xFinal, yFinal);
+                  primerPunto = false;
+                } else {
+                  context.lineTo(xFinal, yFinal);
+                }
+              });
+              pointsStringFinal = pointsStringFinal.trim();
+              herramienta.setAttribute('points', pointsStringFinal);
+              context.closePath();
+              context.fill();
+
+              actualizacionHecha = true;
+            }
           }
         };
 
@@ -110,8 +147,7 @@ export class AreaSimulacionComponent implements OnInit {
       };
 
       // Borrado 
-      animate('x', desplazamiento.x + posicionXInicial, desplazamientoTiempo * 1000);
-      animate('y', desplazamiento.y + posicionYInicial, desplazamientoTiempo * 1000);
+      animate(desplazamientoTiempo * 1000);
 
     })
   }
@@ -134,17 +170,10 @@ export class AreaSimulacionComponent implements OnInit {
 
     // Se actualiza la posicion de la herramienta al inicio de lasimulacion
     let herramienta: SVGRectElement = this.herramienta.nativeElement;
-    let posicionXInicial: number = parseFloat(herramienta.getAttribute("x"));
-    let posicionYInicial: number = parseFloat(herramienta.getAttribute("y"));
+    let posicionXInicial: number = parseFloat(herramienta.getAttribute('points').split(' ')[0].split(',')[0]);
+    let posicionYInicial: number = parseFloat(herramienta.getAttribute('points').split(' ')[0].split(',')[1]);
     this.actualizacionPosicionHerramientaService.sendActualizacionPosicionHerramienta(
       altoAreaSimulacion - posicionYInicial, posicionXInicial);
   }
 
-}
-
-class Goma {
-  x: number;
-  y: number;
-  width: number;
-  height: number;
 }
